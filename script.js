5f4abb19b06ee78ba3d21f7903c5825c71f3ebd2
Shencat <shenzicat@gmail.com>
2022-11-23 16:17:03 +0100

    poprawki

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hi world")
+const rArr = document.querySelector('right')
+console.log (rArr)
+
+
+
+rArr.addEventListener('click', function(){
+    console.log([document.querySelector('carousel-inner').children
+     console.log ( articles )
+])
+
+})
+
+    console.log
+
+    console.log(KLIKNIĘTĘ)
+
+
+let diraction = "left";
+if(diraction == 'left'){
+    console.log("diraction jest left");
+}else{
+    console.log("diraction jest nie jest left");
+}
+
+
+
+if(wysokość > 180){
+    
+}
+
+
+
+
+
+
---
+++ trolololo/Untitled-1.js
@@ -0,0 +1,6 @@
+// stała rarr przypsiujemy do kalsy .right z pobr. z dokumenty HTML
+const rArr = document.querySelector('right')
+rArr.addEventListener('click', function () {
+    const activeElement = activeElement.nextElementSibling
+    if (!nextActiveElement)
+}
